3f1f66a8d284bfe61370a971cdd41b2b1268e57b
dngrcoin <49603937+dngrcoin@users.noreply.github.com>
2020-12-18 12:05:18 +0300

    Update DNGRinstall.sh

--- DNGRinstall.sh
+++ DNGRinstall.sh
@@ -4,11 +4,11 @@
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install software-properties-common build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y && sudo apt-get update -y && sudo add-apt-repository ppa:bitcoin/bitcoin && sudo apt-get update -y && sudo apt-get install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev mc -y
 
-wget https://github.com/dngrcoin/dngrcoin/releases/download/v1.7.3/dngrcoind-for-ubuntu-16.04.tar.gz
+wget https://github.com/dngrcoin/dngrcoin/releases/download/v1.7.4/dngrcoind-64bit-ubuntu16.tar.gz
 
-tar -xvzf dngrcoind-for-ubuntu-16.04.tar.gz
+tar -xvzf dngrcoind-64bit-ubuntu16.tar.gz
 
-rm -rf dngrcoind-for-ubuntu-16.04.tar.gz
+rm -rf dngrcoind-64bit-ubuntu16.tar.gz
 
 mkdir .dngrcoin
 
